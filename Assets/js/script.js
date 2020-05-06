8dad44df4a9b43c5e76e3685d2f549f35fb44c9e
Michael Farris <farris.michael.j@gmail.com>
2020-05-05 18:23:14 -0700

    added variables for the ui elements

---
+++ Assets/js/script.js
@@ -0,0 +1,28 @@
+//start screen
+var startScn = document.querySelector("#startScn");
+var startBtn = document.querySelector("#startBtn");
+var highScoresBtn = document.querySelector("#highScoresBtn");
+//question screen
+var questionScn = document.querySelector("#questionScn");
+var timeLeft = document.querySelector("#timeLeft");
+var currentQuestion = document.querySelector("#currentQuestion");
+var answers = document.querySelector("#answers");
+var results = document.querySelector("#results");
+
+//victory screen
+var victoryScn = document.querySelector("#victoryScn");
+var nameInput = document.querySelector("#nameInput").value;
+var submitBtn = document.querySelector("#submitBtn");
+var score = document.querySelector("#score");
+var playAgainVSBtn = document.querySelector("#playAgainVSBtn");
+var highScoresVSBtn = document.querySelector("#highScoresVSBtn");
+
+//defeat screen
+var defeatScn = document.querySelector("#defeatScn");
+var playAgainDSBtn = document.querySelector("#playAgainDSBtn");
+var highScoresDSBtn = document.querySelector("#highScoresDSBtn");
+
+//high score screen
+var highScoreScn = document.querySelector("#highScoreScn");
+var highScoresList = document.querySelector("#highScoresList");
+var backButton = document.querySelector("#backButton");
