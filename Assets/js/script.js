8bdd566b2f1b3f5bd19ab671ea5d3de925c6b5ff
Michael Farris <farris.michael.j@gmail.com>
2020-05-08 22:25:13 -0700

    working on the question display

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,12 +3,10 @@ var startScn = document.querySelector("#startScn");
 var startBtn = document.querySelector("#startBtn");
 var highScoresBtn = document.querySelector("#highScoresBtn");
 //question screen
-var timeAttackScn = document.querySelector("#timeAttackScn");
-var timeLeft = document.querySelector("#timeLeft");
+var casualScn = document.querySelector("#casualScn");
 var currentQuestion = document.querySelector("#currentQuestion");
 var answers = document.querySelector("#answers");
 var results = document.querySelector("#results");
-var casualScn = document.querySelector("#casualScn");
 var nextBtn = document.querySelector("#nextBtn");
 
 //victory screen
@@ -75,20 +73,24 @@ var casualScores = [
         score: 0
     }
 ];
-
-var quizURL = "https://opentdb.com/api.php?amount=10&type=multiple";
-var wikiURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow";
+var question = "";
+var correctAnswer = "";
+var incorrectAnswers = [];
+var allAnswers = [];
+var shuffledAnswers = [];
 initialize();
+
 function initialize() {
     //loadScores();
     currentIndex = 0;
     timeLeft = 60;
-    //  startScn.style.display = "block";
-    // timeAttackScn.style.display = "none";
-    // victoryScn.style.display = "none";
-    // defeatScn.style.display = "none";
-    // highScoreScn.style.display = "none";
+    startScn.style.display = "block";
+    casualScn.style.display = "none";
+    victoryScn.style.display = "none";
+    defeatScn.style.display = "none";
+    highScoreScn.style.display = "none";
     var queryUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow";
+
 
     var encodedUrl = encodeURIComponent(queryUrl);
     $.ajax({
@@ -99,59 +101,72 @@ function initialize() {
             console.log(data);
         }
     });
-}
-
-function startTimer() {
-    timer = setInterval(function () {
-        timeLeft--;
-        if (timeLeft < 0) {
-            DisplayDefeatScreen();
-            clearInterval(timer);
-        }
-        timeLeftSpan.textContent = timeLeft;
-    }, 1000)
-}
-
-$("#timeAttackBtn").on("click", function () {
-    startTimer();
-    displayTimeAttack();
-});
-
-$("#casualBtn").on("click", function () {
-    displayCasual();
-})
-// Here we are building the URL we need to query the database
-var quizURL = "https://opentdb.com/api.php?amount=10&type=multiple";
-var wikiURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow";
-// We then created an AJAX call
-$.ajax({
-    url: quizURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
-
-function displayTimeAttack() {
 
 }
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-function displayCasual() {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-}
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-function displayQuestionScn(index) {
-    answers.innerHTML = "";
-    currentQuestion.textContent = questions[index].question;
-    for (var i = 0; i < questions[index].choices.length; i++) {
-        var question = document.createElement("li");
-        var questionBtn = document.createElement("button");
-        questionBtn.setAttribute("type", "button");
-        questionBtn.setAttribute("id", i);
-        questionBtn.textContent = questions[index].choices[i];
-        questionBtn.setAttribute("class", "btn btn-primary");
-        question.appendChild(questionBtn);
-        answers.appendChild(question);
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
+
+    return array;
+}
+$("#startBtn").on("click", function () {
+    currentIndex = 0;
+    displayCasualScn(currentIndex);
+})
+
+// Here we are building the URL we need to query the database
+// We then created an AJAX call
+
+function displayCasualScn(index) {
+    var quizURL = "https://opentdb.com/api.php?amount=20&type=multiple";
+    answers.innerHTML = "";
+    $.ajax({
+        url: quizURL,
+        method: "GET"
+    }).then(function (response) {
+        currentQuestion.textContent = response.results[index].question;
+        for (let i = 0; i < 20; i++) {
+            // question = response.results[i].question;
+            correctAnswer = response.results[i].correct_answer;
+            incorrectAnswers = response.results[i].incorrect_answers;
+            allAnswers = incorrectAnswers.push(correctAnswer);
+            shuffledAnswers = shuffle(allAnswers);
+            var choices = document.createElement("li");
+            var choice = document.createElement("button");
+            choice.setAttribute("type", "button");
+            choice.setAttribute("id", i);
+            choice.textContent = shuffledAnswers[i];
+            choice.setAttribute("class", "button");
+            choices.appendChild(choice);
+            answers.appendChild(choices);
+            console.log(question);
+            console.log(shuffledAnswers);
+        }
+    });
+
+    // currentQuestion.textContent = questions[index].question;
+    // for (var i = 0; i < questions[index].choices.length; i++) {
+    //     var question = document.createElement("li");
+    //     var questionBtn = document.createElement("button");
+    //     questionBtn.setAttribute("type", "button");
+    //     questionBtn.setAttribute("id", i);
+    //     questionBtn.textContent = questions[index].choices[i];
+    //     questionBtn.setAttribute("class", "btn btn-primary");
+    //     question.appendChild(questionBtn);
+    //     answers.appendChild(question);
+    // }
 }
 
 answers.addEventListener("click", function (playerAnswer) {
