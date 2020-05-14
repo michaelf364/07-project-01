daf9f609d11526e19dbc077b623a2ea2a7144440
Tanner Nielsen <tan-ali5@outlook.com>
2020-05-13 20:11:41 -0700

    nothing

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -125,10 +125,6 @@ function startAction() {
 }
 
 $("#submitPlayerInitials").on("click", function () {
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     if (pName.value === '')
         alert('Please enter your name !')
     else {
@@ -154,19 +150,11 @@ $("#highScoresDSBtn").on("click", function () {
     previewHighScore();
 });
 
-<<<<<<< HEAD
-function previewHighScore() {
-    var tempScores = JSON.parse(localStorage.getItem("player"));
-    if (tempScores !== null) {
-        highScores = tempScores;
-        alert(JSON.stringify(highScores));
-=======
 function getScoresArray() {
     var array = [];
     for (var c = 1; c <= countOFPlayers; c++) {
         var obj = JSON.parse(localStorage.getItem("player" + c));
         array.push(obj);
->>>>>>> master
     }
     return array;
 }
@@ -214,16 +202,6 @@ $("#nextBtn").on("click", function () {
         resultPerc = 100 - (counterOfNotCorrectAnswer * 20);
         score.innerHTML = 'The test is finished and you scored a ' + resultPerc + '%. You got ' + counterOfCorrectAnswer + ' correct and ' + counterOfNotCorrectAnswer + ' incorrect.';
         score.style.color = "black";
-<<<<<<< HEAD
-        if (resultPerc >= 50)
-            victoryScn.style.display = "contents";
-        else
-            victoryScn.style.display = "contents";
-        // defeatScn
-    }
-    resultsWikiInfo.innerHTML = '';
-});
-=======
         if (resultPerc == 0) {
             casualScn.style.display = "none";
             victoryScn.style.display = "none";
@@ -243,7 +221,6 @@ $("#nextBtn").on("click", function () {
                 highScoreScn.style.display = "none";
                 defeatScn.style.display = "none";
             } else {
->>>>>>> master
 
                 casualScn.style.display = "none";
                 victoryScn.style.display = "none";
